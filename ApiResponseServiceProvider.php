55db403fd0932579dfa123a051d342520668fb9c
Mojtaba Rakhisi <mojtaba.rakhisi@gmail.com>
2020-12-26 18:28:42 +0330

    adding service provider and src folder

---
+++ ApiResponseServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Liateam\ApiResponse;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+}
