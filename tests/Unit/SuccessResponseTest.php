faff19341bd6e4b58033a136d63294ef414c681f
Mojtaba Rakhisi <mojtaba.rakhisi@gmail.com>
2020-12-29 16:18:34 +0330

    success response test refactored

--- tests/Unit/SuccessResponseTest.php
+++ tests/Unit/SuccessResponseTest.php
@@ -2,10 +2,10 @@
 
 namespace Tests\Unit;
 
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
-use Liateam\ApiResponse\Responses\SuccessResponse;
 use Tests\BaseTestCase;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Liateam\ApiResponse\Responses\SuccessResponse;
 
 class SuccessResponseTest extends BaseTestCase
 {
